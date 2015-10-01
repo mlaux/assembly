a82204678ce9c9a20167fba15025fe293601e095
Trent Davies <trentwdb@gmail.com>
2015-10-01 00:49:36 -0500

    added back button for scores screen

--- src/ScoresScreen.js
+++ src/ScoresScreen.js
@@ -36,13 +36,91 @@ var StaticScoresScreen = function() {
             ctx.fillText(score['score'], canvas.width / 2 + canvas.width / 50 + canvas.width / 10, y - canvas.width / 240);
             y += this._getScoreFontSize();
         }
+
+        this.renderBackButton();
+    };
+
+    this.renderBackButton = function() {
+        ctx.globalAlpha = this.clickToContinueOpacity;
+
+        var pos = this._getBackButtonPosition();
+        var dimensions = this._getButtonDimensions();
+
+        var hover = this._isPointInsideBackButton(GameInput.mousePos[0], GameInput.mousePos[1]) && !TransitionManager.isTransitioning();
+
+        if (hover) {
+            ctx.drawImage(
+                globalBackButtonHover,
+                pos[0],
+                pos[1],
+                dimensions[0],
+                dimensions[1]
+            );
+        } else {
+            ctx.drawImage(
+                globalBackButton,
+                pos[0],
+                pos[1],
+                dimensions[0],
+                dimensions[1]
+            );
+        }
+
+        ctx.fillStyle = hover ? '#' + Constants.COLOR_LIGHT_GRAY : '#' + Constants.COLOR_WHITE;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'top';
+        ctx.font = this._getButtonFontSize() + 'px Begok ';
+        ctx.fillText('back', pos[0] + dimensions[0] / 2, pos[1] + dimensions[1]);
+
+        ctx.globalAlpha = 1;
     };
 
     this.click = function(x, y) {
-
+        if (this._isPointInsideBackButton(x, y)) {
+            TransitionManager.startTransition(function() {
+                StateManager.setState(StateManager.lastState);
+            });
+        }
     };
 
     this._getScoreFontSize = function() {
         return canvas.width / 24;
+    };
+
+    this._getBackButtonPosition = function() {
+        var dim = this._getButtonDimensions();
+        return [
+            canvas.width * 0.05,
+            canvas.height - canvas.width * 0.05 - dim[1]
+        ]
+    };
+
+    this._getButtonDimensions = function() {
+        return [
+            canvas.width * 0.0005 * 153,
+            canvas.width * 0.0005 * 128
+        ];
+    };
+
+    this._getButtonFontSize = function() {
+        return canvas.width * 0.0175;
+    };
+
+    this._getButtonFontHeight = function() {
+        return canvas.width * 0.018;
+    };
+
+    this._isPointInsideBackButton = function(x, y) {
+        var pos = this._getBackButtonPosition();
+        var dim = this._getButtonDimensions();
+        dim[1] += this._getButtonFontHeight();
+
+        // top right point of bounding box
+        var point = [
+            pos[0] + dim[0] + pos[0],
+            pos[1] - (canvas.height - pos[1] - dim[1])
+        ];
+
+        return x < point[0] && y >= point[1];
     };
 };
