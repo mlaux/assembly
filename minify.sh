7aecabf54ff6a0e9f0efdc6f8a5d52213b2cca17
Trent Davies <trentwdb@gmail.com>
2015-10-01 22:35:40 -0500

    pushing new mangle stuff even though its not done

--- minify.sh
+++ minify.sh
@@ -2,5 +2,5 @@
 LIB_FILES=`find libs -name "*.js"`
 SRC_FILES=`find src -name "*.js" | grep -v "^src/Initialize"`
 echo $SRC_FILES
-uglifyjs -o centrifuge.min.js $LIB_FILES $SRC_FILES src/Initialize.js
+uglifyjs -o centrifuge.min.js $SRC_FILES src/Initialize.js --mangle toplevel --mangle-props --reserve-domprops --reserved-file reserved.json --source-map min.js.map
 
